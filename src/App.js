6a9fb28c86766a05a42cadebb64406a5092fa1ac
laurabartha <laurabartha@gmail.com>
2022-01-17 12:04:28 -0500

    all my lovely new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import FanInformation from "./FanInformation";
+import PageHeading from "./PageHeading";
 
 function App() {
+  console.log("WOOO APP WAZZUP");
+
+  const ancientBirbsTeam = {
+    id: 1,
+    name: "Torrano Ancient Birbs",
+    city: "Torrano",
+    stadium: {
+      name: "Birbs Nest",
+      capacity: 30000
+    }
+  }
+
+  const modernBirbs = {
+    id: 2,
+    name: "Torrano Modern Birbs",
+    city: "Torrano",
+    stadium: {
+      name: "Birb Roost",
+      capacity: 30001
+    }
+  }
+
+  const sportsTeams = [ancientBirbsTeam, modernBirbs];
+
+  const followTeam = (name) => {
+    console.log(`You are now following that ${name}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PageHeading>
+        <h1> ALL THE SPORTS TEAMS!!</h1>
+        <h2> All The Time</h2>
+      </PageHeading>
+      <div>
+      {
+        sportsTeams.map((team, index)=>{
+          return (
+          <FanInformation
+          key = {team.id} 
+          id = {team.id}
+          teamName={team.name}
+          number = {index}
+          city = {team.city}
+          stadium = {team.stadium}
+          followTeamFunction = {followTeam}
+          />
+          )
+        })
+      }
+      </div>
     </div>
   );
 }
---
+++ src/PageHeading.js
@@ -0,0 +1,11 @@
+const PageHeading = (props) => {
+    return(
+        <header>
+            {/* props stuff here (maybe some components? some jsx? somethign to display! ) */}
+            {props.children}
+            <p> This Will Appeare where you see meeee!!</p>
+        </header>
+    )
+
+}
+export default PageHeading;
